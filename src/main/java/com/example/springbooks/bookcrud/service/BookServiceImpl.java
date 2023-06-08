ac02351beebe8f164ea8c659e2a9edfe17b3a6d7
jonszeto <jonszeto821@gmail.com>
2023-06-08 12:54:07 -0400

    set up jparepository for Book

--- src/main/java/com/example/springbooks/bookcrud/service/BookServiceImpl.java
+++ src/main/java/com/example/springbooks/bookcrud/service/BookServiceImpl.java
@@ -1,42 +1,52 @@
 package com.example.springbooks.bookcrud.service;
 
-import com.example.springbooks.bookcrud.dao.BookDAO;
+import com.example.springbooks.bookcrud.dao.BookRepository;
 import com.example.springbooks.bookcrud.entity.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookServiceImpl implements BookService {
 
-    private final BookDAO bookDAO;
+//    private final BookDAO bookDAO;
 
+    private BookRepository bookRepository;
     @Autowired
-    public BookServiceImpl(BookDAO theBookDAO) {
-        bookDAO = theBookDAO;
+    public BookServiceImpl(BookRepository theBookRepository) {
+        bookRepository = theBookRepository;
     }
 
     @Override
     public List<Book> findAll() {
-        return bookDAO.findAll();
+        return bookRepository.findAll();
     }
 
     @Override
     public Book findById(int id) {
-        return bookDAO.findById(id);
+        Optional<Book> result = bookRepository.findById(id);
+
+        Book theBook = null;
+
+        if (result.isPresent()) {
+            theBook = result.get();
+        }
+
+        return theBook;
     }
 
     @Transactional
     @Override
     public Book save(Book book) {
-        return bookDAO.save(book);
+        return bookRepository.save(book);
     }
 
     @Transactional
     @Override
     public void deleteById(int id) {
-        bookDAO.deleteById(id);
+        bookRepository.deleteById(id);
     }
 }
