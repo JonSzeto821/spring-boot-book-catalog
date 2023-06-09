130e61ea477a137636c9423c2a4820c72244917b
jonszeto <jonszeto821@gmail.com>
2023-06-09 16:21:44 -0400

    create a login page for the app

---
+++ src/main/java/com/example/springbooks/bookcrud/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.springbooks.bookcrud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showCustomLoginPage")
+    public String showCustomLoginPage() {
+
+        return "login/login-form";
+    }
+}
--- src/main/java/com/example/springbooks/bookcrud/security/SecurityConfiguration.java
+++ src/main/java/com/example/springbooks/bookcrud/security/SecurityConfiguration.java
@@ -2,9 +2,11 @@ package com.example.springbooks.bookcrud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
@@ -30,6 +32,26 @@ public class SecurityConfiguration {
                 .build();
 
         return new InMemoryUserDetailsManager(admin, alexis, kephren);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        // restrict access based on the http request
+        httpSecurity.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest()
+                        .authenticated()
+        )
+                // direct user to the custom login form
+                .formLogin(form ->
+                        form
+                                .loginPage("/showCustomLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                );
+
+        return httpSecurity.build();
     }
 
 }
