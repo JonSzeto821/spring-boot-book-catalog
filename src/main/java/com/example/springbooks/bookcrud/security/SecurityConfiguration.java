d66bbabf6ae14419fe264f693bebf4dbd4a2a1c9
jonszeto <jonszeto821@gmail.com>
2023-06-12 13:08:54 -0400

    setup jdbc for user and role details

--- src/main/java/com/example/springbooks/bookcrud/security/SecurityConfiguration.java
+++ src/main/java/com/example/springbooks/bookcrud/security/SecurityConfiguration.java
@@ -3,41 +3,23 @@ package com.example.springbooks.bookcrud.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("{noop}admin")
-                .roles("MEMBER", "ADMIN")
-                .build();
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        UserDetails alexis = User.builder()
-                .username("alexis")
-                .password("{noop}supersecretpassword123")
-                .roles("MEMBER")
-                .build();
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
 
-        UserDetails fabinho = User.builder()
-                .username("fabinho")
-                .password("{noop}supersecretpassword123")
-                .roles("MEMBER", "AUTHOR")
-                .build();
-
-        UserDetails harvey = User.builder()
-                .username("harvey")
-                .password("{noop}supersecretpassword123")
-                .roles("TRIAL_USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, alexis, fabinho, harvey);
+        return jdbcUserDetailsManager;
     }
 
     @Bean
